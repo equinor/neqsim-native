832277fc45a4ab2ff25e128d83f863af9bf329f9
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2025-06-05 06:02:00 +0000

    update

--- java_graal/src/main/java/neqsim/util/WaterDewPoint.java
+++ java_graal/src/main/java/neqsim/util/WaterDewPoint.java
@@ -19,32 +19,35 @@ public class WaterDewPoint {
      */
     @CEntryPoint(name = "calcWaterDewPoint")
     public static double calcWaterDewPoint(IsolateThread thread, double pressure, double ppmWater) {
-        // Create a new thermodynamic system
-
-        SystemInterface testSystem = new SystemSrkCPAstatoil(260.15, pressure);
-        testSystem.addComponent("CO2", 0.02);
-        testSystem.addComponent("nitrogen", 0.01);
-        testSystem.addComponent("methane", 0.9 - ppmWater * 1e-6);
-        testSystem.addComponent("ethane", 0.05);
-        testSystem.addComponent("propane", 0.01);
-        testSystem.addComponent("i-butane", 0.005);
-        testSystem.addComponent("n-butane", 0.005);
-        testSystem.addComponent("water", ppmWater * 1e-6);
-        testSystem.setMixingRule(10);
-        testSystem.setMultiPhaseCheck(true);
-
-        // Perform the water dew point calculation
-        ThermodynamicOperations ops = new ThermodynamicOperations(testSystem);
         try {
-            ops.waterDewPointTemperatureMultiphaseFlash();
+            // Create a new thermodynamic system
+            SystemInterface testSystem = new SystemSrkCPAstatoil(260.15, pressure);
+            testSystem.addComponent("CO2", 0.02);
+            testSystem.addComponent("nitrogen", 0.01);
+            testSystem.addComponent("methane", 0.9 - ppmWater * 1e-6);
+            testSystem.addComponent("ethane", 0.05);
+            testSystem.addComponent("propane", 0.01);
+            testSystem.addComponent("i-butane", 0.005);
+            testSystem.addComponent("n-butane", 0.005);
+            testSystem.addComponent("water", ppmWater * 1e-6);
+            testSystem.setMixingRule(10);
+            testSystem.setMultiPhaseCheck(true);
+
+            // Perform the water dew point calculation
+            ThermodynamicOperations ops = new ThermodynamicOperations(testSystem);
+            try {
+                ops.waterDewPointTemperatureMultiphaseFlash();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            // Get the calculated dew point temperature
+            double dewPointTemperature = testSystem.getTemperature("C");
+            return dewPointTemperature;
         } catch (Exception e) {
             e.printStackTrace();
+            return Double.NaN; // Return NaN if an error occurs
         }
-
-        // Get the calculated dew point temperature
-        double dewPointTemperature = testSystem.getTemperature("C");
-
-        return dewPointTemperature;
     }
 
     /**
@@ -58,29 +61,34 @@ public class WaterDewPoint {
      */
     @CEntryPoint(name = "calcWaterInGas")
     public static double calcWaterInGas(IsolateThread thread, double pressure, double temperature) {
-        // Create a new thermodynamic system
-        double ppmWater = 100.0;
-        SystemInterface testSystem = new SystemSrkCPAstatoil(273.15 + temperature, pressure);
-        testSystem.addComponent("CO2", 0.02);
-        testSystem.addComponent("nitrogen", 0.01);
-        testSystem.addComponent("methane", 0.9 - ppmWater * 1e-6);
-        testSystem.addComponent("ethane", 0.05);
-        testSystem.addComponent("propane", 0.01);
-        testSystem.addComponent("i-butane", 0.005);
-        testSystem.addComponent("n-butane", 0.005);
-        testSystem.addComponent("water", ppmWater * 1e-6);
-        testSystem.setMixingRule(10);
-
-        // Perform the water dew point calculation
-        ThermodynamicOperations ops = new ThermodynamicOperations(testSystem);
         try {
-            ops.saturateWithWater();
+            // Create a new thermodynamic system
+            double ppmWater = 100.0;
+            SystemInterface testSystem = new SystemSrkCPAstatoil(273.15 + temperature, pressure);
+            testSystem.addComponent("CO2", 0.02);
+            testSystem.addComponent("nitrogen", 0.01);
+            testSystem.addComponent("methane", 0.9 - ppmWater * 1e-6);
+            testSystem.addComponent("ethane", 0.05);
+            testSystem.addComponent("propane", 0.01);
+            testSystem.addComponent("i-butane", 0.005);
+            testSystem.addComponent("n-butane", 0.005);
+            testSystem.addComponent("water", ppmWater * 1e-6);
+            testSystem.setMixingRule(10);
+
+            // Perform the water dew point calculation
+            ThermodynamicOperations ops = new ThermodynamicOperations(testSystem);
+            try {
+                ops.saturateWithWater();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            double waterContent = testSystem.getPhase("gas").getComponent("water").getx() * 1e6;
+            return waterContent;
         } catch (Exception e) {
             e.printStackTrace();
+            return Double.NaN; // Return NaN if an error occurs
         }
-        double waterContent = testSystem.getPhase("gas").getComponent("water").getx() * 1e6;
-
-        return waterContent;
     }
 
     /**
@@ -90,9 +98,7 @@ public class WaterDewPoint {
      */
     public static void main(String[] args) {
         // Example usage
-       System.out.println("Calculated water dew point temperature: " );
-
-        System.out.println("Calculated water content in gas: " );
+        System.out.println("Calculated water dew point temperature: ");
+        System.out.println("Calculated water content in gas: ");
     }
-
 }
